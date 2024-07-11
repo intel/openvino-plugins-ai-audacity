59b3f565f20a7e8f18b8f53611c01fddf732e731
Ryan Metcalfe <107415876+RyanMetcalfeInt8@users.noreply.github.com>
2024-07-11 12:46:04 -0400

    music gen: Remove #if 0 dead code blocks related to normalization

--- mod-openvino/OVMusicGenerationLLM.cpp
+++ mod-openvino/OVMusicGenerationLLM.cpp
@@ -212,33 +212,6 @@ bool EffectOVMusicGenerationLLM::DoEffect(
    return EffectBase::DoEffect(settings, finder, projectRate, list, factory, selectedRegion, flags, pAccess);
 }
 
-#if 0
-static void NormalizeSamples(std::shared_ptr<std::vector<float>> samples, WaveTrack* base, float target_rms)
-{
-   auto tmp_tracklist = base->WideEmptyCopy();
-
-   auto iter =
-      (*tmp_tracklist->Any<WaveTrack>().begin())->Channels().begin();
-
-   auto& tmp = **iter++;
-   tmp.Append((samplePtr)samples->data(), floatSample, samples->size());
-
-   auto pTmpTrack = *tmp_tracklist->Any<WaveTrack>().begin();
-   pTmpTrack->Flush();
-
-   float tmp_rms = pTmpTrack->GetRMS(pTmpTrack->GetStartTime(), pTmpTrack->GetEndTime());
-
-   float rms_ratio = target_rms / tmp_rms;
-   {
-      float* pSamples = samples->data();
-      for (size_t si = 0; si < samples->size(); si++)
-      {
-         pSamples[si] *= rms_ratio;
-      }
-   }
-}
-#endif
-
 bool EffectOVMusicGenerationLLM::MusicGenCallback(float perc_complete)
 {
    std::lock_guard<std::mutex> guard(mProgMutex);
@@ -751,22 +724,6 @@ bool EffectOVMusicGenerationLLM::Process(EffectInstance&, EffectSettings& settin
          {
             bool editClipCanMove = GetEditClipsCanMove();
 
-            //Don't normalize until we figure out how we want this to work
-            // (as you need to keep in mind audio continuation, etc.).
-#if 0
-            if (!bNormalized)
-            {
-#define GEN_DB_TO_LINEAR(x) (pow(10.0, (x) / 20.0))
-               float target_rms = GEN_DB_TO_LINEAR(std::clamp<double>(mRMSLevel, -145.0, 0.0));
-               NormalizeSamples(generated_samples_L, track, target_rms);
-               if (generated_samples_R)
-               {
-                  NormalizeSamples(generated_samples_R, track, target_rms);
-               }
-               bNormalized = true;
-            }
-#endif
-
             if (!_AudioContinuationAsNewTrackCheckBox->GetValue())
             {
                //if we can't move clips, and we're generating into an empty space,
@@ -992,27 +949,6 @@ void EffectOVMusicGenerationLLM::DoPopulateOrExchange(
          }
       }
       S.EndMultiColumn();
-
-      // Disable Normalization option until we figure out how we want this to work..
-#if 0
-      S.StartMultiColumn(4, wxLEFT);
-      {
-         S.AddVariableText(XO("Normalize "), false,
-            wxALIGN_CENTER_VERTICAL | wxALIGN_LEFT);
-
-         S.Name(XO("RMS dB"))
-            .Validator<FloatingPointValidator<double>>(
-               2, &mRMSLevel,
-               NumValidatorStyle::ONE_TRAILING_ZERO,
-               -145.0, 0.0)
-            .AddTextBox({}, L"", 10);
-
-         S.AddVariableText(XO("dB"), false,
-            wxALIGN_CENTER_VERTICAL | wxALIGN_LEFT);
-      }
-      S.EndMultiColumn();
-#endif
-
 
       S.StartMultiColumn(2, wxLEFT);
       {
