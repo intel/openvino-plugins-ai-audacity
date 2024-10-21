c925c1cba1388c7d75f171ee483587fab0e1e176
Ryan Metcalfe <ryan.metcalfe@intel.com>
2024-10-21 05:28:31 -0700

    Fix wstring->string conversion for Windows
    Signed-off-by: Ryan Metcalfe <ryan.metcalfe@intel.com>

--- mod-openvino/OVStringUtils.h
+++ mod-openvino/OVStringUtils.h
@@ -4,11 +4,7 @@
 #pragma once
 #include <string>
 #include <locale>
-#ifdef _WIN32 
-#include <windows.h>
-#else
 #include <codecvt>
-#endif
 
 static inline std::string FullPath(std::string base_dir, std::string filename)
 {
@@ -21,15 +17,8 @@ static inline std::string FullPath(std::string base_dir, std::string filename)
 }
 
 static inline std::string wstring_to_string(const std::wstring& wstr) {
-#ifdef _WIN32 
-   int size_needed = WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-   std::string strTo(size_needed, 0);
-   WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-   return strTo;
-#else 
    std::wstring_convert<std::codecvt_utf8<wchar_t>> wstring_decoder;
    return wstring_decoder.to_bytes(wstr);
-#endif 
 }
 
 // WA for OpenVINO locale caching issue (https://github.com/openvinotoolkit/openvino/issues/24370)
