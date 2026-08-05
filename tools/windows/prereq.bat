3c2509a1ba9ceb17b8513c2702537c4f73ecc8db
Ryan Metcalfe <ryan.metcalfe@intel.com>
2026-08-05 07:30:53 -0700

    ci: Update to OV GenAI 2026.3.0 release

--- tools/windows/prereq.bat
+++ tools/windows/prereq.bat
@@ -9,14 +9,14 @@ set "bat_path=%~dp0"
 set LIBTORCH_PACKAGE_URL="https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-2.4.1%%%%2Bcpu.zip"
 set LIBTORCH_PACKAGE_256SUM=e7b8d0b3b958d2215f52ff5385335f93aa78e42005727e44f1043d94d5bfc5dd
 
-set OPENVINO_GENAI_PACKAGE_URL=https://storage.openvinotoolkit.org/repositories/openvino_genai/packages/pre-release/2026.3.0.0rc2/openvino_genai_windows_2026.3.0.0rc2_x86_64.zip
-set OPENVINO_GENAI_PACKAGE_256SUM=90eac26a1041cac477f80fe44699c76d007952d7f9164a72971900b332d50e16
+set OPENVINO_GENAI_PACKAGE_URL=https://storage.openvinotoolkit.org/repositories/openvino_genai/packages/2026.3/windows/openvino_genai_windows_2026.3.0.0_x86_64.zip
+set OPENVINO_GENAI_PACKAGE_256SUM=3d01daee17953a1b787841cff6a4d19b5de1d06d996af9433969139b2b5ea657
 
 :::::::::::::::::::::::::::::
 ::  GIT Repo Configuration ::
 :::::::::::::::::::::::::::::
 set AUDACITY_REPO_CLONE_URL=https://github.com/audacity/audacity.git
-set AUDACITY_REPO_CHECKOUT=release-3.7.7
+set AUDACITY_REPO_CHECKOUT=release-3.7.8
 
 ::::::::::::::::::::::::::::::::::::::::::::::::
 :: Download, verify, and extract the packages ::
